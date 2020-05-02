69e5d7787b2c250755ccada153efa5da4da4f5f3
RefaatMahmoud <RefaatAishDev@gmail.com>
2020-05-02 03:45:11 +0200

    intial project

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+   <div> Hello World From App</div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
+new Vue({
+    el: '#app',
+    render : h => h(App)
+});
