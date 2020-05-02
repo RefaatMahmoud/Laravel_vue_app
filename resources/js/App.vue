9ff7f9261aa5536e53be5c59e574a937114cb0cc
RefaatMahmoud <RefaatAishDev@gmail.com>
2020-05-02 23:40:05 +0200

    Create Template & instantiate Vue router

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,37 @@
 <template>
-   <div> Hello World From App</div>
+    <div>
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+            <Sidebar></Sidebar>
+            <!-- Content Wrapper -->
+            <div class="d-flex flex-column" id="content-wrapper">
+
+                <div id="content">
+                    <Navbar></Navbar>
+                    <router-view></router-view>
+                </div>
+                <Footer></Footer>
+            </div>
+        </div>
+
+
+    </div>
 </template>
 
 <script>
+    import Sidebar from "./components/partials/Sidebar";
+    import Navbar from "./components/partials/Navbar";
+    import Statistics from "./components/partials/Statistics";
+    import Footer from "./components/partials/Footer";
+
     export default {
-        name: "App"
+        name: "App",
+        components: {
+            Sidebar,
+            Navbar,
+            Statistics,
+            Footer
+        }
     }
 </script>
 
---
+++ resources/js/views/Categories.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container-fluid">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Categories Management</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table cellspacing="0" class="table table-bordered" id="dataTable" width="100%">
+                        <thead>
+                        <tr>
+                            <th width="20%">ID</th>
+                            <th width="40%">Name</th>
+                            <th width="30%">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>System Architect</td>
+                            <td>
+                                <button class="btn btn-info">
+                                    <i class="fa fa-eye"></i>
+                                    Show
+                                </button>
+                                <button class="btn btn-primary">
+                                    <i class="fa fa-edit"></i>
+                                    Edit
+                                </button>
+                                <button class="btn btn-danger">
+                                    <i class="fa fa-times"></i>
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Categories"
+    }
+</script>
+
+<style scoped>
+
+</style>
